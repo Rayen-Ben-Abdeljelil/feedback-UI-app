1d445c2bc099c19754fc0fd80b10d1c22e7058a0
Rayen-Ben-Abdeljelil <rayane.benabdeljelil@eniso.u-sousse.tn>
2025-02-03 23:07:54 +0100

    added the json server part

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,19 +1,20 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
 
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-    if(! feedback || feedback.length === 0 ) {
+    if((! feedback || feedback.length === 0) && !isLoading) {
         return <p>No Feedbacks yet.</p>
     }
     
-    return (
+    return isLoading ? <Spinner /> :
+    (
     <div className='feedback-list'>
         <AnimatePresence>
         {feedback.map((item) => (
---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import spinner from '../assets/spinner.webp' 
+
+function Spinner() {
+  return (
+    <img src={spinner} alt="Loading..." style={{width: '200px', margin: 'auto', display: 'block'}}
+     />
+      
+    
+  )
+}
+
+export default Spinner
